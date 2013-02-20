a8baee7c824a058d5ff1019109ed3e0ab4a9f20e
mgarbacz <mich.garbacz@gmail.com>
2013-02-19 20:53:29 -0600

    Testing for loading plugin

---
+++ test/plugin.js
@@ -0,0 +1,13 @@
+var Dustjs = require('dustjs-linkedin');
+
+describe('Plugin', function() {
+  var plugin;
+
+  beforeEach(function() {
+    plugin = new Plugin({});
+  });
+
+  it('should be an object', function() {
+    expect(plugin).to.be.ok;
+  });
+});
