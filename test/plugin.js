3fc8e6da3e92a5dca83927b6ca70a777ad60ca25
mgarbacz <mich.garbacz@gmail.com>
2013-02-20 22:38:02 -0600

    Full tests

--- test/plugin.js
+++ test/plugin.js
@@ -1,4 +1,4 @@
-var Dustjs = require('dustjs-linkedin');
+var Dust = require('dustjs-linkedin');
 
 describe('Plugin', function() {
   var plugin;
@@ -9,5 +9,24 @@ describe('Plugin', function() {
 
   it('should be an object', function() {
     expect(plugin).to.be.ok;
+  });
+
+  it('should has #compile method', function() {
+    expect(plugin.compile).to.be.an.instanceof(Function);
+  });
+
+  it('should compile and produce valid result', function(done) {
+    var content = '<h1>Hello {{name}}!</h1>';
+    var expected = '<h1>Hello Batman!</h1>';
+
+    plugin.compile(content, 'template.dust', function(error, data) {
+      var compiled = eval(data)
+      dust.loadSource(compiled);
+      dust.render('template', { 'name': 'Batman' }. function(error, output) {
+        expect(error).not.to.be.ok;
+        expect(ouput).to.equal(expected);
+        done();
+      });
+    });
   });
 });
